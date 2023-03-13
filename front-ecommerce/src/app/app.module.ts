a731bf5ecc979ed56d22b5a06afac70f9a3a44b9
Johan <jojmoralespa@unal.edu.co>
2023-03-13 15:57:24 -0500

    creación de componentes nav-bar y homepage

--- front-ecommerce/src/app/app.module.ts
+++ front-ecommerce/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import {MenubarModule} from 'primeng/menubar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-ecommerce/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  items!: MenuItem[];
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Registro de clientes',
+                icon: 'pi pi-fw pi-file-edit',
+                routerLink: "registro"
+            },
+            {
+                label: 'Clientes',
+                icon: 'pi pi-fw pi-users',
+                routerLink: 'clientes'
+            },
+            {
+              label: 'Agendamientos',
+              icon: 'pi pi-fw pi-book',
+              routerLink: 'agendamientos'
+          }
+        ];
+    }
+
+}
